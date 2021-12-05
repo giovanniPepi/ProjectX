41d31a079c1948b688f37e699a373eda8b647a15
gerijeb <gtpepi@hotmail.com>
2021-12-05 19:06:04 -0300

    Add initial JavaScript

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-const dado = document.getElementById('input1');
-const para = document.getElementById('result1');
+const dado = document.getElementById('textRico');
+const para = document.getElementById('calcRico');
 
 function preço (num) {
     return num * num;
@@ -7,5 +7,30 @@ function preço (num) {
 
 dado.onchange = function () {
     const num = parseFloat(dado.value);
-    para.textContent = preço(num) + ' quadrado';
-}
+
+    if (isNaN(num)) {
+        para.textContent = 'Please type a number!';
+    } else switch (true) {
+                case num < 0:
+                    para.textContent = 'So you have debts in XCH? Woah';
+                    break;
+                case num === 0:
+                    para.textContent = 'Go on get some XCH!';
+                    break;
+                case num <= 5:
+                    para.textContent = 'Sorry man no lambo for u :(';
+                    break;
+                case num <= 100:
+                    para.textContent = 'AUDI is a cheaper versions of Lambo...';
+                    break;
+                case num <= 1000:
+                    para.textContent = 'GOING FOR THAT LAMBO!';
+                    break;
+                case num <= 10000:
+                    para.textContent = 'SPARE SOME LAMBO FOR US!';
+                    break;
+
+                default:
+                    para.textContent = 'Bro WTF you typed?';
+            }
+    }
